e9257635209d8e9f369df5902580ea97a148bf88
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-23 23:02:44 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+  
+
+  return (
+    <div className=' bg-gradient-to-r from-[#f4dbf9] to-[#dddcfe]'>
+     {/* head */}
+     <Navbar/>
+    </div>
+  )
+}
+
+export default App
